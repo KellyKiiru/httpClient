2442d44e64d24547b6777b7457b5deba3d6daf10
KellyKiiru <infowithkiiru@gmail.com>
2023-08-21 15:35:28 +0300

    event binding = click event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'movies-app';
   today = new Date()  
-  imgUrl = 'https://images.unsplash.com/photo-1682685797527-63b4e495938f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60'
-  imgWidth= 100;
-  price = 20;
 
-  sellingPrice = 18.99;
+  timeNow = new Date()
+  
+  setTimeToNow(){
+    this.timeNow = new Date()
+  }
 
-  attributevalue = 'movies-value'
-
-  bgColor = 'purple'
 }
