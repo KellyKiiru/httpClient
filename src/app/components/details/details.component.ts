70133b7e39a578c180474631a1958536849a51b4
KellyKiiru <infowithkiiru@gmail.com>
2023-08-21 14:53:11 +0300

    changes

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, AfterContentInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {
+ 
+export class DetailsComponent implements OnInit, OnChanges, AfterContentInit, AfterViewInit{
+
+  constructor(){
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges is called");
+  }
+
+  ngOnInit(): void {
+    console.log("ngOnInit is called");
+  }
+
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit is called");
+  }
+
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit is called");
+  }
 
 }
