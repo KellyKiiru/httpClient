97a450774b07b2bafe2541bec5d66ab35b9f6795
KellyKiiru <infowithkiiru@gmail.com>
2023-08-21 14:52:23 +0300

    extrapolation practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'movies-app';
+  today = new Date()  
+  hackathonUrl = 'https://agribiz.kenyacic.org/wp-content/uploads/2022/11/AgriBiz%20Program%20Year%204%20Call%20for%20Applications%20guidelines%202022%20_%20Updated%2016-11-2022.pdf'
+  price = 20;
+
+  sellingPrice = 18.99;
 }
