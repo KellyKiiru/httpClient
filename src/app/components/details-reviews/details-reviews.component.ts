805e198908ec320e3ce8869890581f777efb28c9
KellyKiiru <infowithkiiru@gmail.com>
2023-08-24 19:23:55 +0300

    movieRates array

--- src/app/components/details-reviews/details-reviews.component.ts
+++ src/app/components/details-reviews/details-reviews.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./details-reviews.component.css']
 })
 export class DetailsReviewsComponent {
-
+  movieRates:any[]=[
+    {
+      rate:7.4,
+      review:"This will be the review section of our movies section and sometimes it gets too big. That is why we will use an accordion",
+      author:'Matt Damon',
+      usefulCount: 83,
+      totalCount:120
+    },
+    {
+      rate:6.3,
+      review:"This will be the review section of our movies section and sometimes it gets too big. That is why we will use an accordion",
+      author:'Matt Damon',
+      usefulCount: 10,
+      totalCount:120
+    },
+    {
+      rate:9.1,
+      review:"This will be the review section of our movies section and sometimes it gets too big. That is why we will use an accordion",
+      author:'Matt Damon',
+      usefulCount: 90,
+      totalCount:100
+    }
+  ]
 }
