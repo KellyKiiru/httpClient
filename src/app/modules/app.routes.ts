a32690d9c79032671401e823ea643e2b3941b9ff
KellyKiiru <infowithkiiru@gmail.com>
2023-08-24 12:17:34 +0300

    add search route

--- src/app/modules/app.routes.ts
+++ src/app/modules/app.routes.ts
@@ -2,11 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule,Routes } from "@angular/router";
 import { DetailsComponent } from "../components/details/details.component";
 import { HomeComponent } from "../components/home/home.component";
+import { SearchComponent } from "../components/search/search.component";
 
 const routes:Routes = [
     {path:'', component:HomeComponent},
     {path:'home', component:HomeComponent},
     {path:'details', component:DetailsComponent},
+    {path:'search', component:SearchComponent},
+    // {path:'feedback', component:FeedbackComponent}
 ]
 
 @NgModule({
