33678a8a2d3e2019a6158e820c8e219d55b65384
KellyKiiru <infowithkiiru@gmail.com>
2023-08-24 20:46:49 +0300

    consume json data from movieSummary using loadMovieSummary()

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, OnChanges, SimpleChanges, AfterContentInit, AfterViewInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -7,34 +8,28 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./details.component.css']
 })
  
-export class DetailsComponent implements OnInit, OnChanges, AfterContentInit, AfterViewInit{
+export class DetailsComponent implements OnInit{
   movieId = ''
   
   stars = ['Star 1','Star 2','star 3'];
   genres = ['Genre 1','Genre 2','Genre 3'];
   directors = ['Director 1','Director 2','Director 3'];
 
-  constructor(private _activatedRoute: ActivatedRoute){
+  constructor(private _activatedRoute: ActivatedRoute, private httpClient:HttpClient){
     this._activatedRoute.params.subscribe((p) =>{
       this.movieId = p['id']
       console.log(this.movieId)
     })
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log("ngOnChanges is called");
+  loadMovieSummary(){
+    this.httpClient.get('assets/data/movieSummary.json').subscribe((data:any)=>{
+      console.log("Summary = ", data);
+    })
   }
 
   ngOnInit(): void {
-    console.log("ngOnInit is called");
-  }
-
-  ngAfterContentInit(): void {
-    console.log("ngAfterContentInit is called");
-  }
-
-  ngAfterViewInit(): void {
-    console.log("ngAfterViewInit is called");
-  }
+    this.loadMovieSummary()
+  }  
 
 }
