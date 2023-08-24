c36637142e2e2deeb1d70c2d6ad55856ef7eb93e
KellyKiiru <infowithkiiru@gmail.com>
2023-08-24 19:58:57 +0300

    import HttpClientModule and declare it in the Imports array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -46,7 +47,8 @@ import { MyIfDirective } from './directives/my-if.directive';
     BrowserModule,
     AppRoutesModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
