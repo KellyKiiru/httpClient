cf5cf4c0ec4179cac36238f6cee4710b1dfa3bde
KellyKiiru <infowithkiiru@gmail.com>
2023-08-24 19:40:05 +0300

    define similarMovies array

--- src/app/components/details-movies/details-movies.component.ts
+++ src/app/components/details-movies/details-movies.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./details-movies.component.css']
 })
 export class DetailsMoviesComponent {
-
+  
+  similarMovies:any []=[
+    {
+      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
+      title:'Meg',
+      rate:4.3
+    },
+    {
+      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
+      title:'Movie 2',
+      rate:4.3
+    },
+    {
+      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
+      title:'Movie 3',
+      rate:4.3
+    },
+    {
+      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
+      title:'Movie 4',
+      rate:4.3
+    },
+    {
+      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
+      title:'Movie 5',
+      rate:4.3
+    },
+    {
+      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
+      title:'Movie 6',
+      rate:4.3
+    },
+    {
+      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
+      title:'Movie 7',
+      rate:4.3
+    },
+    {
+      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
+      title:'Movie 8',
+      rate:4.3
+    }
+  ]
 }
