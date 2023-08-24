7c49f116e97f16e29ebb8c0322e0749d8147bd36
KellyKiiru <infowithkiiru@gmail.com>
2023-08-24 21:37:38 +0300

    configure search functionality

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpBackend, HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -6,12 +7,26 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit{
+
   movieTitle = ''
-  constructor(private _activatedRoute: ActivatedRoute){
+  searchResults:any[]=[]
+
+
+  constructor(private _activatedRoute: ActivatedRoute, private httpClient: HttpClient){
     this._activatedRoute.params.subscribe((p)=>{
       this.movieTitle = p['movieTitle'];
       console.log(this.movieTitle)
     });
+  }
+
+ searchMovies(){
+  this.httpClient.get<any[]>('assets/data/searchMovies.json').subscribe((data:any[])=>{
+    this.searchResults = data
+  })
  }
+
+  ngOnInit(): void {
+    this.searchMovies()
+  }
 }
