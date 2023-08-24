dbfe8a378fb905db7106127d8fbba34d0f863d02
KellyKiiru <infowithkiiru@gmail.com>
2023-08-24 15:18:31 +0300

    set styling for ngStyles derective

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  errorColor = "text-danger";
-  successColor ="text-success"
+
+  // Record is the js equivalent of python dictionaries
+  // Which have key:value pairs
+  currentStyles : Record<string,string> = {};
+  
   isSuccess = true;
+  isCentered = true;
+  isLarge = true;
+
+  constructor(){
+    this.currentStyles = {
+      'text-align':this.isCentered? 'center':'',
+      'color':this.isSuccess? 'green':'red',
+      'font-size':this.isLarge?'large':'small',
+    };
+  }
 }
