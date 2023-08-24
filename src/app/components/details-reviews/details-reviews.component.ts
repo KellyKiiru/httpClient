d5874a5dc2ba39005749962fc45a5fc8cfcb59b1
KellyKiiru <infowithkiiru@gmail.com>
2023-08-24 21:15:27 +0300

    configure ts to get data from json file

--- src/app/components/details-reviews/details-reviews.component.ts
+++ src/app/components/details-reviews/details-reviews.component.ts
@@ -1,32 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-details-reviews',
   templateUrl: './details-reviews.component.html',
   styleUrls: ['./details-reviews.component.css']
 })
-export class DetailsReviewsComponent {
-  movieRates:any[]=[
-    {
-      rate:7.4,
-      review:"This will be the review section of our movies section and sometimes it gets too big. That is why we will use an accordion",
-      author:'Matt Damon',
-      usefulCount: 83,
-      totalCount:120
-    },
-    {
-      rate:6.3,
-      review:"This will be the review section of our movies section and sometimes it gets too big. That is why we will use an accordion",
-      author:'Matt Damon',
-      usefulCount: 10,
-      totalCount:120
-    },
-    {
-      rate:9.1,
-      review:"This will be the review section of our movies section and sometimes it gets too big. That is why we will use an accordion",
-      author:'Matt Damon',
-      usefulCount: 90,
-      totalCount:100
-    }
-  ]
+export class DetailsReviewsComponent implements OnInit{
+
+  constructor(private httpClient:HttpClient){}
+
+  movieRates:any[]=[]
+
+  loadMovieRates(){
+    this.httpClient.get<any[]>('assets/data/movieRates.json').subscribe((data:any[])=>{
+      console.log(data);
+      this.movieRates=data;
+    })
+  }
+
+  ngOnInit(): void {
+    this.loadMovieRates()
+  }
 }
