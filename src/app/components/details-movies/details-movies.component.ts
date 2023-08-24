2bca70bf80a592a89a9e5d7b78cdacddeaec826e
KellyKiiru <infowithkiiru@gmail.com>
2023-08-24 21:15:46 +0300

    configure ts to get data from json file

--- src/app/components/details-movies/details-movies.component.ts
+++ src/app/components/details-movies/details-movies.component.ts
@@ -1,52 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-details-movies',
   templateUrl: './details-movies.component.html',
   styleUrls: ['./details-movies.component.css']
 })
-export class DetailsMoviesComponent {
-  
-  similarMovies:any []=[
-    {
-      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
-      title:'Meg',
-      rate:4.3
-    },
-    {
-      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
-      title:'Movie 2',
-      rate:4.3
-    },
-    {
-      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
-      title:'Movie 3',
-      rate:4.3
-    },
-    {
-      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
-      title:'Movie 4',
-      rate:4.3
-    },
-    {
-      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
-      title:'Movie 5',
-      rate:4.3
-    },
-    {
-      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
-      title:'Movie 6',
-      rate:4.3
-    },
-    {
-      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
-      title:'Movie 7',
-      rate:4.3
-    },
-    {
-      imageUrl:'../../../assets/images/doug-watanabe-p060CZWRGSc-unsplash.jpg',
-      title:'Movie 8',
-      rate:4.3
-    }
-  ]
+export class DetailsMoviesComponent implements OnInit{
+
+
+  constructor(private httpClient:HttpClient){}
+
+  similarMovies:any[]=[]
+
+  loadSimilarMovies(){
+    this.httpClient.get<any[]>('assets/data/similarMovies.json').subscribe((data:any[])=>{
+      console.log(data);
+      this.similarMovies=data;
+    })
+  }
+
+  ngOnInit(): void {
+    this.loadSimilarMovies()
+  }
 }
