a2246f57bb2caa32b0002d62154a5762d3561cb8
KellyKiiru <infowithkiiru@gmail.com>
2023-08-24 12:53:00 +0300

    add routes with id parameter

--- src/app/modules/app.routes.ts
+++ src/app/modules/app.routes.ts
@@ -3,12 +3,18 @@ import { RouterModule,Routes } from "@angular/router";
 import { DetailsComponent } from "../components/details/details.component";
 import { HomeComponent } from "../components/home/home.component";
 import { SearchComponent } from "../components/search/search.component";
+import { ActorsComponent } from "../components/actors/actors.component";
+import { DetailsMoviesComponent } from "../components/details-movies/details-movies.component";
+import { DetailsReviewsComponent } from "../components/details-reviews/details-reviews.component";
 
 const routes:Routes = [
     {path:'', component:HomeComponent},
     {path:'home', component:HomeComponent},
-    {path:'details', component:DetailsComponent},
-    {path:'search', component:SearchComponent},
+    {path:'details/:id', component:DetailsComponent},
+    {path:'details/actors/:id', component:ActorsComponent},
+    {path:'details/movies/:id', component:DetailsMoviesComponent},
+    {path:'details/reviews/:id', component:DetailsReviewsComponent},
+    {path:'search/:movieTitle', component:SearchComponent},
     // {path:'feedback', component:FeedbackComponent}
 ]
 
