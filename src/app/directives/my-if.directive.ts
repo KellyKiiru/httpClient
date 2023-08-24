e541ec0625742dbdddbfd6c7ba18f8dc5fe068bd
KellyKiiru <infowithkiiru@gmail.com>
2023-08-24 19:52:55 +0300

    create custom structural directive

--- src/app/directives/my-if.directive.ts
+++ src/app/directives/my-if.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appMyIf]'
 })
 export class MyIfDirective {
 
-  constructor() { }
+  constructor(private templateRef:TemplateRef<any>, private viewContainer:ViewContainerRef) { }
 
+  @Input() set appMyIf(condition:boolean){
+    if(condition){
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainer.clear();
+    }
+  }
 }
